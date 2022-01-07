e86edeafb3762e32cb24a519ee9870d44bcce73b
Wesley Stedman <wesleystedman@gmail.com>
2022-01-06 17:53:10 -0800

    Implement result sprite display

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,28 +4,40 @@ function Results(props) {
   return (
     <div className='Results-container'>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>No Effect</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>No Effect - {props.resultArrays['0'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['0'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>0.25x Effective</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>0.25x Effective - {props.resultArrays['0.25'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['0.25'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>0.5x Effective</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>0.5x Effective - {props.resultArrays['0.5'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['0.5'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>1.0x Effective</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>1.0x Effective - {props.resultArrays['1'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['1'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>2.0x Effective</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>2.0x Effective - {props.resultArrays['2'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['2'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
       <div className='Results-section'>
-        <div className='Results-section-header'><h3>4.0x Effective</h3></div>
-        <div className='Results-section-content'>Sprites go here</div>
+        <div className='Results-section-header'><h3>4.0x Effective - {props.resultArrays['4'].length}</h3></div>
+        <div className='Results-section-content'>
+          {props.resultArrays['4'].map(sprite => <img src={sprite} alt="pokemon sprite" key={sprite} className="sprite" />)}
+        </div>
       </div>
     </div>
   );
