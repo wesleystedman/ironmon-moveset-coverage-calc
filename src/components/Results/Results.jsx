ef5097481afcc884dd51d5088abf7a6e9a8220a1
Wesley Stedman <wesleystedman@gmail.com>
2022-01-10 13:29:56 -0800

    Add results header counts

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -3,6 +3,32 @@ import React from "react";
 function Results(props) {
   return (
     <div className='Results-container'>
+      <div className='Results-header'>
+        <div className='Results-header-container'>
+          <h4>0.0x</h4>
+          <div className='Results-header-count'>{props.resultArrays['0'].length}</div>
+        </div>
+        <div className='Results-header-container'>
+          <h4>0.25x</h4>
+          <div className='Results-header-count'>{props.resultArrays['0.25'].length}</div>
+        </div>
+        <div className='Results-header-container'>
+          <h4>0.5x</h4>
+          <div className='Results-header-count'>{props.resultArrays['0.5'].length}</div>
+        </div>
+        <div className='Results-header-container'>
+          <h4>1.0x</h4>
+          <div className='Results-header-count'>{props.resultArrays['1'].length}</div>
+        </div>
+        <div className='Results-header-container'>
+          <h4>2.0x</h4>
+          <div className='Results-header-count'>{props.resultArrays['2'].length}</div>
+        </div>
+        <div className='Results-header-container'>
+          <h4>4.0x</h4>
+          <div className='Results-header-count'>{props.resultArrays['4'].length}</div>
+        </div>
+      </div>
       <div className='Results-section'>
         <div className='Results-section-header'><h3>No Effect - {props.resultArrays['0'].length}</h3></div>
         <div className='Results-section-content'>
