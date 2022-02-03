17a0127b78a182181d334e99ecc9a9fb37fedc0a
Wesley Stedman <wesleystedman@gmail.com>
2022-02-02 18:14:06 -0800

    Expand type 'checkbutton' effective click area to fill the border - Add onClick handler to containing divs - handler creates a click event on child checkbox

--- src/components/TypeControls/TypeControls.jsx
+++ src/components/TypeControls/TypeControls.jsx
@@ -13,10 +13,15 @@ function TypeControls(props) {
       : props.setSelectedMoveTypes(props.selectedMoveTypes.concat([event.target.value]))
   }
 
+  // Redirect a click on the div part of the type 'button' to the checkbox, like the label does (afaict)
+  function handleDivButtonClick(event) {
+    if (event.target.tagName === 'DIV') event.target.firstChild.click();
+  }
+
   return (
     <div className='Controls-types'>
       {types.map((type, index) => (
-        <div className="type-button" key={type}>
+        <div className="type-button" key={type} onClick={handleDivButtonClick}>
           <input type="checkbox" name={`type-${type.toLowerCase()}`} id={`type-${type.toLowerCase()}`} value={index} onChange={handleChange} />
           <label htmlFor={`type-${type.toLowerCase()}`}>{type}</label>
         </div>
