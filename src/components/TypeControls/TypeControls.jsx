2a450e1a470572b7c1aae06a1f92f7ab6bdeda56
Wesley Stedman <wesleystedman@gmail.com>
2022-02-04 17:45:54 -0800

    Add colors to the type buttons

--- src/components/TypeControls/TypeControls.jsx
+++ src/components/TypeControls/TypeControls.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import './TypeControls.css';
 
-const types = [
+const TYPES = [
   'Normal', 'Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel',
   'Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark', 'Fairy'
+];
+
+const TYPE_COLORS = [
+  {backgroundColor: '#a8a878', border: '1px solid #6d6d4e'},
+  {backgroundColor: '#c03028', border: '1px solid #7d1f1a'},
+  {backgroundColor: '#a890f0', border: '1px solid #6d5e9c'},
+  {backgroundColor: '#a040a0', border: '1px solid #682a68'},
+  {backgroundColor: '#e0c068', border: '1px solid #927d44'},
+  {backgroundColor: '#b8a038', border: '1px solid #786824'},
+  {backgroundColor: '#a8b820', border: '1px solid #6d7815'},
+  {backgroundColor: '#705898', border: '1px solid #493963'},
+  {backgroundColor: '#b8b8d0', border: '1px solid #787887'},
+  {backgroundColor: '#f08030', border: '1px solid #9c531f'},
+  {backgroundColor: '#6890f0', border: '1px solid #445e9c'},
+  {backgroundColor: '#78c850', border: '1px solid #4e8234'},
+  {backgroundColor: '#f8d030', border: '1px solid #a1871f'},
+  {backgroundColor: '#f85888', border: '1px solid #a13959'},
+  {backgroundColor: '#98d8d8', border: '1px solid #638d8d'},
+  {backgroundColor: '#7038f8', border: '1px solid #4924a1'},
+  {backgroundColor: '#705848', border: '1px solid #49392f'},
+  {backgroundColor: '#ee99ac', border: '1px solid #9b6470'},
 ];
 
 function TypeControls(props) {
@@ -20,8 +41,8 @@ function TypeControls(props) {
 
   return (
     <div className='Controls-types'>
-      {types.map((type, index) => (
-        <div className="type-button" key={type} onClick={handleDivButtonClick}>
+      {TYPES.map((type, index) => (
+        <div className="type-button" style={TYPE_COLORS[index]} key={type} onClick={handleDivButtonClick}>
           <input type="checkbox" name={`type-${type.toLowerCase()}`} id={`type-${type.toLowerCase()}`} value={index} onChange={handleChange} />
           <label htmlFor={`type-${type.toLowerCase()}`}>{type}</label>
         </div>
